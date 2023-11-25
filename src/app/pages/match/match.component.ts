d7d0d347ee0ab373044b37c30d51be21ab8be00c
Lucas Ramos <codinglucao@gmail.com>
2023-11-24 23:46:38 -0300

    style: change page style

--- src/app/pages/match/match.component.ts
+++ src/app/pages/match/match.component.ts
@@ -2,23 +2,22 @@ import { Component } from '@angular/core';
 
 export interface PeriodicElement {
   id: number;
-  campeonato: string;
   time1: string;
   time2: string;
-  resultado: string;
+  vencedor: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { id: 1, campeonato: 'campeonato 1', time1: 'Time A', time2: 'Time B', resultado: '3 X 0' },
-  { id: 2, campeonato: 'campeonato 2', time1: 'Time A', time2: 'Time B', resultado: '3 X 0' },
-  { id: 3, campeonato: 'campeonato 3', time1: 'Time A', time2: 'Time B', resultado: '3 X 0' },
-  { id: 4, campeonato: 'campeonato 4', time1: 'Time A', time2: 'Time B', resultado: '3 X 0' },
-  { id: 5, campeonato: 'campeonato 5', time1: 'Time A', time2: 'Time B', resultado: '3 X 0' },
-  { id: 6, campeonato: 'campeonato 6', time1: 'Time A', time2: 'Time B', resultado: '3 X 0' },
-  { id: 7, campeonato: 'campeonato 7', time1: 'Time A', time2: 'Time B', resultado: '3 X 0' },
-  { id: 8, campeonato: 'campeonato 8', time1: 'Time A', time2: 'Time B', resultado: '3 X 0' },
-  { id: 9, campeonato: 'campeonato 9', time1: 'Time A', time2: 'Time B', resultado: '3 X 0' },
-  { id: 10, campeonato: 'campeonato 10', time1: 'Time A', time2: 'Time B', resultado: '3 X 0' },
+  { id: 1, time1: 'Time A', time2: 'Time B', vencedor: '-' },
+  { id: 2, time1: 'Time A', time2: 'Time B', vencedor: '-' },
+  { id: 3, time1: 'Time A', time2: 'Time B', vencedor: '-' },
+  { id: 4, time1: 'Time A', time2: 'Time B', vencedor: '-' },
+  { id: 5, time1: 'Time A', time2: 'Time B', vencedor: '-' },
+  { id: 6, time1: 'Time A', time2: 'Time B', vencedor: '-' },
+  { id: 7, time1: 'Time A', time2: 'Time B', vencedor: '-' },
+  { id: 8, time1: 'Time A', time2: 'Time B', vencedor: '-' },
+  { id: 9, time1: 'Time A', time2: 'Time B', vencedor: '-' },
+  { id: 10, time1: 'Time A', time2: 'Time B', vencedor: '-' },
 ];
 
 @Component({
