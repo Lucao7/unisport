f90bbeba5e88892d7f77fa117ea0e27ce82e97e7
Fabio Vilela <fabiovilela123@gmail.com>
2023-11-25 00:55:09 -0300

    feat: add register team for the championship

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -14,6 +14,7 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatMenuModule} from '@angular/material/menu';
 
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MomentDateAdapter } from '@angular/material-moment-adapter';
@@ -47,6 +48,7 @@ const MY_DATE_FORMAT = {
     MatDialogModule,
     MatTableModule,
     MatProgressSpinnerModule,
+    MatMenuModule
   ],
   providers: [
     { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
