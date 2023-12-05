9a39a4ad2e1102eb3121fab888e0f819c5f33d47
Lucas Ramos <codinglucao@gmail.com>
2023-12-04 23:25:14 -0300

    feat: integrate match screen

--- src/app/pages/match/match.component.ts
+++ src/app/pages/match/match.component.ts
@@ -1,21 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 
 import { DialogDefineWinnerComponent } from './dialog-define-winner/dialog-define-winner.component';
+import { ChampionshipService } from 'src/app/_services/championship/championship.service';
+import { Match } from 'src/app/models/match';
 
 @Component({
   selector: 'app-match',
   templateUrl: './match.component.html',
   styleUrls: ['./match.component.scss'],
 })
-export class MatchComponent {
-  constructor(public dialog: MatDialog) {}
+export class MatchComponent implements OnInit {
 
-  openDialogDefineWinner(): void {
-    const DIALOG_DEFINE_WINNER = this.dialog.open(DialogDefineWinnerComponent);
+  championshipName : string = '';
+  matches: Match[] = [];
 
-    DIALOG_DEFINE_WINNER.afterClosed().subscribe((result) => {
-      console.log('O modal foi aberto');
+  constructor(
+    public dialog: MatDialog,
+    private route: ActivatedRoute,
+    private championshipService: ChampionshipService
+  ) { }
+
+  ngOnInit() {
+    let championshipId: any = this.route.snapshot.paramMap.get('id');
+    championshipId = parseInt(championshipId);
+    this.championshipService.getChampionshipById(championshipId).subscribe({
+      next: (data) => {
+        this.championshipName = data.nome;
+        if (data.partidas)
+          this.matches = data.partidas
+      },
+      error: (error) => console.error(error),
+    });
+  }
+
+  openDialogDefineWinner(match: Match): void {
+    const DIALOG_DEFINE_WINNER = this.dialog.open(DialogDefineWinnerComponent, {
+      width: '50dvh',
+      data: match
     });
   }
 }
