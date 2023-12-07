2bc240773051f42d16ded693efd88b223bb2b061
Lucas Ramos <codinglucao@gmail.com>
2023-12-07 04:09:46 -0300

    style: change subtitle depending on user role

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/_services/user/user.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  cardSubtitle: string = 'Ver'
+
+  constructor(
+    private userService: UserService
+  ) { }
+
+  ngOnInit(): void {
+    this.getUserRole();
+  }
+
+  getUserRole() {
+    if(this.userService.getCurrentUser().admin)
+      this.cardSubtitle = 'Gerenciar';
+  }
 
 }
